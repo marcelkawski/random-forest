5596f132e0e8fbabf1e614ca9ae9b54b95bfcda2
marcelkawski <marcel.kawski1@gmail.com>
2021-11-11 21:30:45 +0100

    Add handling csv format files and add new dataset

--- main.R
+++ main.R
@@ -1,5 +1,7 @@
 source('randomForest.R')
 
-cm1 = getRFConfMatrix(path = 'data/telescope/data', doc_type = 'txt')
-cm2 = getRFConfMatrix(path = 'data/dryBean/Dry_Bean_Dataset.xlsx',
-                     doc_type = 'xlsx', header = TRUE)
+cm1 = getRFConfMatrix(path = 'data/telescope/data', doc_type = 'txt',
+                      header = FALSE)
+cm2 = getRFConfMatrix(path = 'data/dryBean/data.xlsx',
+                     doc_type = 'xlsx')
+cm3 = getRFConfMatrix(path = 'data/spam/data.csv', doc_type = 'csv')
--- randomForest.R
+++ randomForest.R
@@ -6,11 +6,13 @@ library(readxl)
 
 
 getRFConfMatrix <- function(path, doc_type, output_attr = 'cl', 
-                            header = FALSE, sep = ',', dec = '.') {
+                            header = TRUE, sep = ',', dec = '.') {
   if (doc_type == 'txt')
     data = read.delim(path, header = header, sep = sep, dec = dec)
   else if (doc_type == 'xlsx')
     data = as.data.frame(read_excel(path = path, col_names = header))
+  else if (doc_type == 'csv')
+    data = read.table(path, header = header, sep = sep)
 
   colnames(data)[ncol(data)] = 'cl'
   data$cl = factor(data$cl)
