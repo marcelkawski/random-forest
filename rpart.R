cd71d29ee92fc40ffa96e59a78750bb9d2403df7
marcelkawski <marcel.kawski1@gmail.com>
2021-11-12 16:45:11 +0100

    Correct rpart.R

--- rpart.R
+++ rpart.R
@@ -1,37 +1,18 @@
-library(plyr)
-library(tidyverse)
-library(dplyr)
-library(caret)
 library(rpart)
-library(rpart.plot)
+library(caret)
 
+source('utils.R')
 
-dat <- read_csv("data/spam/data.csv")
-glimpse(dat)
+getRpartConfMatrix = function(data, split_ratio = .8) {
+  sample = sample.split(data$cl, SplitRatio = split_ratio)
+  train_data = subset(data, sample == TRUE)
+  test_data  = subset(data, sample == FALSE)
+  
+  tree = rpart(cl~., train_data, method = "class")
+  prediction = predict(tree, newdata = test_data, type = 'class')
+  
+  print(length(prediction))
+  print(length(test_data$cl))
 
-colnames(dat)[ncol(dat)] = 'cl'
-
-set.seed(100)
-
-trainRowNumbers <- createDataPartition(dat$cl, p=0.7, list=FALSE)
-train <- dat[trainRowNumbers,]
-test <- dat[-trainRowNumbers,]
-dim(train); dim(test) 
-
-tree_model = rpart(
-  cl~ ., 
-  data = train, 
-  method="class", 
-  minsplit = 10,
-  minbucket=3
-) 
-
-summary(tree_model)
-prp(tree_model)
-
-
-PredictCART_train = predict(tree_model, data = train, type = "class")
-table(train$cl, PredictCART_train)
-
-PredictCART = predict(tree_model, newdata = test, type = "class")
-table(test$cl, PredictCART)
+  return (confusionMatrix(prediction, test_data$cl))
+}
